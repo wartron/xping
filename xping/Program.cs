a32af33fbaa732517ff46fae06a9c3a0d974daae
Will Wharton <willmwharton@gmail.com>
2015-02-20 09:14:44 -0800

    xping code

---
+++ xping/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XPinger xp = new XPinger();
+
+            xp.PingHost("google.com");
+
+            var name = Console.ReadLine();
+        }
+
+    }
+}
---
+++ xping/XPinger.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace xping
+{
+    public class XPinger{
+    
+        int delayMs = 1000;
+
+        public XPinger()
+        {
+
+        }
+
+        public bool PingHost(string nameOrAddress)
+        {
+            int pings = 4;
+
+            bool pingable = false;
+            Ping pinger = new Ping();
+
+            string hostnamestring = "127.0.0.1 [testasasdasdasd]";
+            string stx = String.Format("\nPinging {0} with 32 bytes of data:\n",hostnamestring);
+            Console.Write(stx);
+
+            while (pings>0)
+            {
+                pings--;
+                try
+                {
+                    PingReply reply = pinger.Send(nameOrAddress);
+
+                    pingable = reply.Status == IPStatus.Success;
+
+                    string st = String.Format("Reply from {0}: time={1}ms\n", reply.Address.ToString(), reply.RoundtripTime.ToString());
+                    Console.Write(st);
+
+                    System.Threading.Thread.Sleep(delayMs);
+                }
+                catch (PingException)
+                {
+                    // Discard PingExceptions and return false;
+                    Console.Write("failed");
+                }
+
+            }
+
+            return pingable;
+        }
+
+    }
+}
